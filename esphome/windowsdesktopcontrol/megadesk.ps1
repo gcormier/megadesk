ac552d2c330edd9599f2d25ca6c80862b4bf2361
AnAnalogGuy <70239317+AnAnalogGuy@users.noreply.github.com>
2023-04-01 18:46:37 +0200

    Update megadesk.ps1

--- esphome/windowsdesktopcontrol/megadesk.ps1
+++ esphome/windowsdesktopcontrol/megadesk.ps1
@@ -1,2 +1,3 @@
-$url = "http://[IP OR DNS OF YOUR ESP DEVICE]/button/desk_position_$($args[0])/press"
+$yourESPDevice = "[IP OR DNS OF YOUR ESP DEVICE]"
+$url = $yourESPDevice + "/button/desk_position_$($args[0])/press"
 Invoke-RestMethod -Method 'Post' -Uri $url
