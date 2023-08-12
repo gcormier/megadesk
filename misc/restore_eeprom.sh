2cecb08b65696c8a5db345272c7355867e9b8796
Greg Cormier <gcormier@gmail.com>
2023-08-12 18:40:01 -0400

    cleanup

--- misc/restore_eeprom.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# why doesn't this work? we can write flash no problem, we can read eeprom ok, but can't write it.
-# do we need to erase first??
-./avrdude.exe -p attiny841 -c usbtiny -B2  -U eeprom:w:eeprom_fixed.eep:i
-
