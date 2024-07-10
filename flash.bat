48134d18629eb5bd399271ba749c9f7366d76788
Greg Cormier <gcormier@gmail.com>
2024-07-10 13:17:25 -0400

    Changes fuses to use BOD

--- flash.bat
+++ flash.bat
@@ -1,8 +1,8 @@
 SET avrdudeconf="C:\Users\Greg\AppData\Local\arduino15\packages\ATTinyCore\hardware\avr\1.2.3/avrdude.conf"
 
 avrdude "-C%avrdudeconf%" -c usbtiny -p t841 -U lfuse:w:0xe2:m
-avrdude "-C%avrdudeconf%" -c usbtiny -p t841 -U hfuse:w:0xd6:m
-avrdude "-C%avrdudeconf%" -c usbtiny -p t841 -U efuse:w:0xfe:m
+avrdude "-C%avrdudeconf%" -c usbtiny -p t841 -U hfuse:w:0xd4:m
+avrdude "-C%avrdudeconf%" -c usbtiny -p t841 -U efuse:w:0xf4:m
 
 avrdude "-C%avrdudeconf%" -v -pattiny841 -cusbtiny -Uflash:w:megadesk.hex:i
 
